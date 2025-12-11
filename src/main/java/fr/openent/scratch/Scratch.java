433983bb358650ca18086f9470d6e6dc5e120489
Junior Bernard <junior.bernard@opendigitaleducation.com>
2025-12-11 15:24:34 +0100

    feat: #RBACK-117, kubernetes migration

--- src/main/java/fr/openent/scratch/Scratch.java
+++ src/main/java/fr/openent/scratch/Scratch.java
@@ -3,6 +3,7 @@ package fr.openent.scratch;
 import fr.openent.scratch.controllers.FileController;
 import fr.openent.scratch.cron.ScratchCron;
 import fr.wseduc.cron.CronTrigger;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.eventbus.EventBus;
 import org.entcore.common.events.EventStore;
@@ -10,6 +11,11 @@ import org.entcore.common.events.EventStoreFactory;
 import org.entcore.common.http.BaseServer;
 import org.entcore.common.storage.Storage;
 import org.entcore.common.storage.StorageFactory;
+
+import java.text.ParseException;
+
+import static io.vertx.core.Future.failedFuture;
+import static io.vertx.core.Future.succeededFuture;
 
 public class Scratch extends BaseServer {
 
@@ -24,22 +30,36 @@ public class Scratch extends BaseServer {
 
 	@Override
 	public void start(Promise<Void> startPromise) throws Exception {
-		super.start(startPromise);
+		final Promise<Void> promise = Promise.promise();
+		super.start(promise);
+		promise.future()
+			.compose(e -> initScratch())
+			.onComplete(startPromise)
+			.onFailure(th -> log.error("[Geogebra@Geogebra::start] Fail to start Geogebra", th))
+			.onSuccess(e -> log.info("[Geogebra@Geogebra::start] Fail to start Geogebra"));
+	}
 
+	public Future<Void> initScratch() {
 		scratchSchema = config.getString("db-schema");
 		scratchURL = config.getString("scratch-url");
 		scratchSessionDuration = config.getString("scratch-session-duration", "1 DAY");
 		final EventBus eb = getEventBus(vertx);
-		final Storage storage = new StorageFactory(vertx, config).getStorage();
+		return StorageFactory.build(vertx, config)
+		.compose(storageFactory -> {
+			final Storage storage = storageFactory.getStorage();
 
-		EventStore eventStore = EventStoreFactory.getFactory().getEventStore(Scratch.class.getSimpleName());
+			final EventStore eventStore = EventStoreFactory.getFactory().getEventStore(Scratch.class.getSimpleName());
 
-		addController(new FileController(eb, storage, eventStore));
+			addController(new FileController(eb, storage, eventStore));
 
-		// cron
-		ScratchCron scratchCron = new ScratchCron();
-		new CronTrigger(vertx, config.getString("scratch-cron", "0 */10 * * * ? *")).schedule(scratchCron);
-		startPromise.tryComplete();
-		startPromise.tryFail("[Geogebra@Geogebra::start] Fail to start Geogebra");
+			// cron
+			ScratchCron scratchCron = new ScratchCron();
+            try {
+                new CronTrigger(vertx, config.getString("scratch-cron", "0 */10 * * * ? *")).schedule(scratchCron);
+            } catch (ParseException e) {
+                return failedFuture(e);
+            }
+            return succeededFuture();
+		});
 	}
 }
