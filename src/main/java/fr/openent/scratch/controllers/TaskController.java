591495210438f9171714fa2854062d10e03b62b9
mariusestaque <marius.estaque@opendigitaleducation.com>
2026-03-05 18:20:41 +0100

    chore: exposing cron tasks to be triggered by api call

---
+++ src/main/java/fr/openent/scratch/controllers/TaskController.java
@@ -0,0 +1,25 @@
+package fr.openent.scratch.controllers;
+
+import fr.openent.scratch.cron.ScratchCron;
+import fr.wseduc.rs.Post;
+import fr.wseduc.webutils.http.BaseController;
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+public class TaskController extends BaseController {
+	protected static final Logger log = LoggerFactory.getLogger(TaskController.class);
+
+	final ScratchCron scratchCron;
+
+	public TaskController(ScratchCron scratchCron) {
+		this.scratchCron = scratchCron;
+	}
+
+	@Post("api/internal/scratch-cron")
+	public void runScratchCron(HttpServerRequest request) {
+		log.info("Triggered scratch task");
+		scratchCron.handle(0L);
+		render(request, null, 202);
+	}
+}
